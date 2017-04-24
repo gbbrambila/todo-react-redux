5acde597c5a5b13a5380c423b7f1fd7bf9fa9b2e
gbbrambila <gb.brambila@gmail.com>
2017-04-24 11:44:47 +1000

    Created Todo Component

---
+++ src/components/Todo/TodoComponent.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import TodoItem from './TodoItem';
+import NewTodo from './NewTodo';
+
+import { 
+  addTodo, 
+  toggleTodo, 
+  removeTodo 
+} from '../../redux/todo/actions'; 
+ 
+const VisibleTodoApp = connect(
+  (state) => {
+    return {
+      todos: state.todos
+    }
+  }, 
+  (dispatch) => {
+    return {
+      addTodo: (text) => dispatch(addTodo(text)),
+      toggleTodo: (id) => dispatch(toggleTodo(id)),
+      removeTodo: (id) => dispatch(removeTodo(id))
+    }
+  }
+)
+class TodoApp extends React.Component {
+
+    constructor(props) {
+        super();
+    }
+
+    listTodos() {
+        return this.props.todos.map( (todo, index) => {
+        return <TodoItem 
+                    key={ todo.id } 
+                    todo={ todo } 
+                    toggleTodo={ this.props.toggleTodo } 
+                    removeTodo={ this.props.removeTodo } />
+        });
+    }  
+
+    render() {
+        return (
+            <div>
+                <h1>React - Redux</h1>
+                <NewTodo addTodo={ this.props.addTodo } />
+                <ul> { this.listTodos() } </ul>
+            </div>
+        )
+    }
+
+}
+
+export default VisibleTodoApp(TodoApp);
