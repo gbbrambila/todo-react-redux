0ac605c7282f3030cb4f05a54b814d5e6d1d3876
gbbrambila <gb.brambila@gmail.com>
2017-04-24 11:27:54 +1000

    Removed unused files Created basic app structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Basic Todo React Redux App</h1> 
+      </div>
+    );
+  }
+}
+
+export default App;
